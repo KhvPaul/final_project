dc49daa50ea487780e550ea3dfc190322af4a2c7
Paul Khv <paul.khvaliuk@icloud.com>
2022-05-06 14:57:03 +0300

    maybe fixed

--- shop/app/utils/admin/__init__.py
+++ shop/app/utils/admin/__init__.py
@@ -9,9 +9,9 @@ def init_admin(app):
     admin.init_app(app, index_view=AdminHomeView())
 
     # # if U received troubles with blueprints naming, just comment this block
-    # admin.add_view(RoleModelAdmin(Role, db.session))
-    # admin.add_view(UserModelAdmin(User, db.session))
-    # admin.add_view(AuthorModelAdmin(Author, db.session))
-    # admin.add_view(BookModelAdmin(Book, db.session))
-    # admin.add_view(GenreModelAdmin(Genre, db.session))
-    # admin.add_view(OrderModelAdmin(Order, db.session))
+    admin.add_view(RoleModelAdmin(Role, db.session))
+    admin.add_view(UserModelAdmin(User, db.session))
+    admin.add_view(AuthorModelAdmin(Author, db.session))
+    admin.add_view(BookModelAdmin(Book, db.session))
+    admin.add_view(GenreModelAdmin(Genre, db.session))
+    admin.add_view(OrderModelAdmin(Order, db.session))
